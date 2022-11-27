2477e0b67aa21cd8ba5ed7e0cf0532e3eb8479bd
Mhmd Houmani <mhoumani742@gmail.com>
2022-11-27 12:02:25 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      xs: "380px",
+      ss: "450px",
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [],
+};
